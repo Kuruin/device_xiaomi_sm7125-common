4757845fa730492208a7175c182816a0585f98cc
PeacE-Boi <manavjit9@gmail.com>
2021-07-09 17:03:57 +0000

    Revert "sm7125-common: Nuke livedisplay"
    This reverts commit 9a5f641efec6d8793cf104b5bc4713b779b0d3cc.

--- common.mk
+++ common.mk
@@ -237,6 +237,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm7125
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
